25c6a535aca000d8fc5c5f68d5279181fbbf1895
Bamdad Shamaei <bamshamaei@gmail.com>
2020-08-03 17:01:47 -0400

    Update DailyPrft.py

--- DailyPrft.py
+++ DailyPrft.py
@@ -4,7 +4,7 @@ import yfinance as yf
 from numpy import nan
 
 T = pd.read_csv("assets.csv")
-T["Quant"] = 0
+T["Quantity"] = 0
 T["last_buy"] = T["last_price"] = nan  # Index: Asst_name,Ass_quantity,Last_price
 
 
@@ -18,13 +18,13 @@ def init():
         yf.download(t["Assets"].tolist()).swaplevel(1, 0, 1).to_csv("HIS.csv")
     s = pd.read_csv("HIS.csv", header=[0, 1], index_col=0)
 
-    for i in t["Assets"]:
-        s[i, "STA"] = s[i, "Close"].rolling(5).mean()
-        s[i, "LTA"] = s[i, "Close"].rolling(200).mean()
-        s[i, "HV"] = (s[i, "High"] < s[i, "High"].shift(1)) &\
-                     (s[i, "High"].shift(1) < s[i, "High"].shift(2))
-        s[i, "LV"] = (s[i, "Low"] < s[i, "Low"].shift(1)) & \
-                     (s[i, "Low"].shift(1) < s[i, "Low"].shift(2))
+    for assets in t["Assets"]:
+        s[assets, "STA"] = s[assets, "Close"].rolling(5).mean()
+        s[assets, "LTA"] = s[assets, "Close"].rolling(200).mean()
+        s[assets, "HV"] = (s[assets, "High"] < s[assets, "High"].shift(1)) & \
+                          (s[assets, "High"].shift(1) < s[assets, "High"].shift(2))
+        s[assets, "LV"] = (s[assets, "Low"] < s[assets, "Low"].shift(1)) & \
+                          (s[assets, "Low"].shift(1) < s[assets, "Low"].shift(2))
     s.to_csv("HIS_USE.csv")
 
 
@@ -43,9 +43,9 @@ def calc(days, cash, gen_csv):
     p = pd.DataFrame(index=S.index[-days:], columns=["AV", "C"])  # Date: Asst_val ,cash, total
 
     for day in range(days):
-        v = cash_total / 5
 
         for items in T.index.values:
+            v = cash_total / 5
 
             pos = day - days
 
@@ -72,18 +72,20 @@ def calc(days, cash, gen_csv):
                     T.iat[items, 1] += q
                     T.iat[items, 2] = temp.iat[pos, 1]
 
-        T["V"] = T["Quant"] * T["last_price"]
+        T["V"] = T["Quantity"] * T["last_price"]
         p.iat[day, 0] = T["V"].sum()
         p.iat[day, 1] = cash_total
 
     p["TV"] = p["AV"] + p["C"]
 
     if gen_csv:
-        p.to_csv("P.csv")
+        p.to_csv(f'p-{days}-{cash}.csv')
     return (f'cash={p.iat[-1, 1]},'
             f'assets={p.iat[-1, 0]},'
             f'p%={(p.iat[-1,-1] - cash) / cash}')
 
 
 if __name__ == '__main__':
-    print(calc(5000, 15000, True))
+    i = input("").split(" ")
+    print(calc(int(i[0]), float(i[1]), bool(i[2])))
+    input("")
